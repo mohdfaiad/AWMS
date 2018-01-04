389cf7b4dcc1930ebf77a2e5cfaf95e50fec39a0
jpandaconnor <connor@jpanda.org>
2018-01-04 23:35:03 +0000

    Started to add the database stuff.

--- unit_awms_main.pas
+++ unit_awms_main.pas
@@ -5,7 +5,7 @@ unit unit_awms_main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus, IniFiles;
 
 type
 
@@ -24,8 +24,17 @@ type
 
   end;
 
+const
+  C_DB_SECTION = 'DB_INFORMATION';
+
 var
   XWindow: TXWindow;
+
+  INI: TINIFile;
+  DB_IP, DB_NAME, DB_USER, DB_PASS: String;
+  DB_PORT: integer;
+
+  DB_SETUP_PREVIOUS: LongInt;
 
 implementation
 
@@ -42,7 +51,31 @@ end;
 
 procedure TXWindow.FormCreate(Sender: TObject);
 begin
+  INI := TINIFile.Create('DB.ini');
 
+  // Confirm if we have setup a connection to the database
+
+  try
+    DB_IP := INI.ReadString(C_DB_SECTION, 'DB_IP', 'NULL');
+
+    DB_SETUP_PREVIOUS := CompareText(DB_IP, 'NULL');
+
+    // The database has not been setup and we will have to display some sort
+    // of modal in order to let the user setup any details
+    if DB_SETUP_PREVIOUS = 0 then
+
+    // The database has been setup and we can retrieve some of the values
+    // that are needed to connect
+    else
+      DB_NAME := INI.ReadString(C_DB_SECTION, 'DB_NAME', 'awms');
+      DB_USER := INI.ReadString(C_DB_SECTION, 'DB_USER', 'administrator');
+      DB_PASS := INI.ReadString(C_DB_SECTION, 'DB_PASS', 'password');
+      DB_PORT := INI.ReadInteger(C_DB_SECTION, 'DB_PORT', 3306);
+
+      Write('TESTTT');
+  finally
+         INI.Free;
+  end;
 end;
 
 
